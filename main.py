44b37786a7c2e8ca01d1ad19868d75e3f549f852
Donovansmith2003 <donovan.smith@students.fhu.edu>
2024-02-03 23:30:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-#Remove pass and complete the code
 def check_character(word, index):
-   pass
-    
+
     if word[index].isalpha():
         return "letter"
     elif word[index].isdigit():
@@ -15,4 +13,4 @@ if __name__ == '__main__':
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
-    print(check_character('happy birthday!', 14))    print(check_character('happy birthday!', 14))
+    print(check_character('happy birthday!', 14))
